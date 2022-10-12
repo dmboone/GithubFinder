f23c031deaa29a8d87537b6d3d013a4cbd1df0e2
Destiny Boone <destaj.db@gmail.com>
2022-10-12 15:02:03 -0400

    fetching data

---
+++ github.js
@@ -0,0 +1,15 @@
+class Github {
+    constructor(){
+        this.client_id = '9fd783085eb3c97339b2';
+        this.client_secret = '120f99314046ca88866f2d3764b7ffc105398cdf';
+    }
+
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const profile = await profileResponse.json();
+
+        return {
+            profile // same as profile : profile
+        }
+    }
+}

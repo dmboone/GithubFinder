677230a84f4ac6cda1d572d094cb409fa7871f12
Destiny Boone <destaj.db@gmail.com>
2022-10-12 16:34:53 -0400

    Fetch and display repos

--- github.js
+++ github.js
@@ -1,15 +1,20 @@
 class Github {
     constructor(){ // Register app with github and then enter client id and client secret
         this.client_id = '9fd783085eb3c97339b2'; 
-        this.client_secret = '120f99314046ca88866f2d3764b7ffc105398cdf';
+        this.client_secret = '0e88c5636590bbfd8115aade165dd2003c6ff027';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile // same as profile : profile
+            profile, // same as profile : profile
+            repos // same as repos : repos
         }
     }
 }
